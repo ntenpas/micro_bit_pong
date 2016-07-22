c96690868c7392f0818b638819796d84aabdd86b
Nolan Tenpas <ntenpasdeveloper@gmail.com>
2016-07-21 19:24:12 -0500

    working on wall bounce and paddle bounce

--- pong.py
+++ pong.py
@@ -2,6 +2,70 @@ import radio
 import microbit
 from microbit import *
 
+x = 0
+bx = 0
+by = 0
+time = 0
+# 0=NE, 1=SE, 2=SW, 3=NW
+direction = 1
+
+def paddleBounce(leftside):
+  global direction
+  if leftside == True:
+    direction = 3
+  else:
+    direction = 0
+
+def wallBounce():
+  global bx,by,direction
+  # move ball back to before wall crossing
+  if direction == 0:
+    bx-=1
+    by+=1
+  elif direction == 1:
+    bx-=1
+    by-=1
+  elif direction == 2:
+    bx+=1
+    by-=1
+  elif direction == 3:
+    bx+=1
+    by+=1
+  direction = -1
+
+def moveBall():
+  global bx, by
+  if by == 4:
+    if bx == x:
+      paddleBounce(True)
+    elif bx == (x+1):
+      paddleBounce(False)
+  if bx < 0 or bx > 4 or by < 0 or by > 4:
+    uart.write("wall")
+    wallBounce()
+    return
+  uart.write("hi")
+  # calculate next pos
+  if direction == 0:
+    bx+=1
+    by-=1
+  elif direction == 1:
+    bx+=1
+    by+=1
+  elif direction == 2:
+    bx-=1
+    by+=1
+  elif direction == 3:
+    bx-=1
+    by-=1
+
+def drawBall():
+  global time
+  if time == 100:
+    moveBall()
+    time = 0
+  display.set_pixel(bx,by,9)
+
 def drawPaddle(x):
   display.set_pixel(x,4,9)
   display.set_pixel(x+1,4,9)
@@ -21,12 +85,13 @@ def getX():
 display.on()
 display.clear()
 radio.on()
-x = 0
 while True:
-  uart.write(str(x) + "\r\n")
+  #uart.write(str(x) + "\r\n")
   x = getX()
   display.clear()
   drawPaddle(x)
+  drawBall()
+  time+=1
   if button_a.is_pressed():
     radio.send('msg')
     display.show(Image.HAPPY)
